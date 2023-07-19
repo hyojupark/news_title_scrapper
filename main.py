7d693f77a44807e2b45a3dfae21e958cff0bb578
hyojupark <gkwk011@gmail.com>
2023-07-19 16:29:46 +0900

    데이터 수집 파이프라인 수정

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import io
 
 from google.cloud import storage
 from google.oauth2 import service_account
-# from google.cloud import bigquery
-# from google.oauth2 import service_account
 import pandas as pd
 from fastavro import writer, parse_schema
 
@@ -21,9 +19,8 @@ def clean_html(raw_html: str) -> str:
 
 def get_news_data():
     # credentials = service_account.Credentials.from_service_account_file('credentials/hyoju_service_account.json')
-    # client = bigquery.Client(credentials=credentials, project=credentials.project_id)
-
     # naver_api_credentials = json.load(open('credentials/hyoju_naver_search_api.json'))
+
     naver_api_credentials = {
         'id': os.environ['NAVER_SEARCH_API.ID'],
         'secret': os.environ['NAVER_SEARCH_API.SECRET']
@@ -74,53 +71,19 @@ def news_data_save_to_gcs(request):
                          & (df['pubDate'] < end_date.timestamp() * 1000)]
     print(filtered_df)
 
-    # credentials = service_account.Credentials.from_service_account_file('credentials/hyoju_service_account.json')
-    # client = storage.Client(project=credentials.project_id)
-    client = storage.Client(project=os.environ['PROJECT.ID'])
-    bucket = client.get_bucket(os.environ['BUCKET.NAME'])
+    if not filtered_df.empty:
+        # credentials = service_account.Credentials.from_service_account_file('credentials/hyoju_service_account.json')
+        # client = storage.Client(project=credentials.project_id)
+        client = storage.Client(project=os.environ['PROJECT.ID'])
+        bucket = client.get_bucket(os.environ['BUCKET.NAME'])
 
-    bytesio = io.BytesIO()
-    writer(bytesio, parsed_schema, filtered_df.to_dict('records'))
-    start_date_str = start_date.strftime('%Y%m%d%H%M%S')
-    end_date_str = end_date.strftime('%Y%m%d%H%M%S')
-    bucket.blob(f'raw_data/{start_date_str}_{end_date_str}.avro').upload_from_file(bytesio, rewind=True)
+        bytesio = io.BytesIO()
+        writer(bytesio, parsed_schema, filtered_df.to_dict('records'))
+        start_date_str = start_date.strftime('%Y%m%d%H%M%S')
+        end_date_str = end_date.strftime('%Y%m%d%H%M%S')
+        bucket.blob(f'raw_data/{start_date_str}_{end_date_str}.avro').upload_from_file(bytesio, rewind=True)
 
     return {'status': 'success'}
 
 
-
-# print(type(resp['items'][0]['pubDate']), resp['items'][0]['pubDate'])
-# pub_date_dt = datetime.strptime(resp['items'][0]['pubDate'], "%a, %d %b %Y %H:%M:%S %z")
-# print(type(pub_date_dt), pub_date_dt)
-
-# print(resp.status_code)
-# print(resp.json())
-
-# df = pd.DataFrame()
-# news_list = []
-# for news in resp['items']:
-#     title = news['title']
-#     description = news['description']
-#     original_link = news['originallink']
-#     link = news['link']
-#     pub_date = news['pubDate']
-
-#     print('-'*50)
-#     print(title)
-#     print(description)
-#     print(original_link)
-#     print(link)
-#     print(pub_date)
-
-#     news_list.append({'title': title, 'description': description, 'original_link': original_link, 'link': link, 'pub_date': pub_date}, ignore_index=True)
-
-# df = pd.DataFrame(resp['items'])
-# print(df)
-# df['title'] = df['title'].apply(lambda x: clean_html(x))
-# df['description'] = df['description'].apply(lambda x: clean_html(x))
-# df['pubDate'] = df['pubDate'].apply(lambda x: datetime.strptime(x, "%a, %d %b %Y %H:%M:%S %z"))
-# print(df)
-
-# table_id = 'hyoju-387406.naver_news.search_list_v2'
-# job = client.load_table_from_dataframe(df, table_id)
-# job.result()
+news_data_save_to_gcs('test')
